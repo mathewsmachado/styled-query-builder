f62d866eb1a2cff804cca8893a3b00812f48854e
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-05-20 01:36:36 -0300

    chore: add rollup
    since lint-staged don't respect configuration files or cli constraints defined into package.json, had to fix it before commiting

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+const tsconfigOverride = {
+  exclude: [
+    'node_modules',
+    'build',
+    'coverage',
+    'src/**/*.test.ts',
+    'src/**/*.test.tsx',
+    'src/**/*.test.js',
+    'src/**/*.test.jsx',
+    '*.config.*',
+    'setupTests.ts',
+  ],
+};
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: pkg.module, format: 'esm', sourcemap: true },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+  ],
+  external: [...Object.keys(pkg.peerDependencies)],
+  plugins: [
+    typescript({ tsconfigOverride }),
+    terser({ format: { comments: false } }),
+  ],
+};
