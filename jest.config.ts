6d36977be521f4049e4c97fadedc5d5172101e65
Mathews Machado <mathews.machadoamorim@gmail.com>
2021-07-19 23:08:22 -0300

    chore: fix tests coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   bail: 0,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/*'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
   transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src/'],
   modulePaths: ['<rootDir>/src/'],
 };
